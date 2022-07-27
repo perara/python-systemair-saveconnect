b1b8bbe1f10b2bec3ccba69f82b0622086a7dcb1
Per-Arne Andersen <per@sysx.no>
2022-07-27 15:20:52 +0200

    Build fixes.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-python3 setup.py sdist bdist_wheel
-python3 -m pip install --user --upgrade twine
-twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+python3 -m pip install --upgrade twine
+python3 -m pip install --upgrade build
+python3 -m build
+python3 -m twine upload --repository pypi dist/*
+
